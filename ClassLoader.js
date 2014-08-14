ac395b5297c056cdbd94a6aae096c8a5eeb23fab
teotigraphix <mschmalle@teotigraphix.com>
2014-08-14 05:23:20 -0400

    Added - FRAMEWORK_4_BITWIG_VERSION constant.

--- ClassLoader.js
+++ ClassLoader.js
@@ -3,6 +3,10 @@
 // (c) 2014
 // Licensed under GPLv3 - http://www.gnu.org/licenses/gpl.html
 
+// Anytime a commit changes API or funtionality, increment the revision number in the commit
+// Add to the https://github.com/teotigraphix/Framework4Bitwig/wiki/Changes wiki page
+var FRAMEWORK_4_BITWIG_VERSION = "0.1.0";
+
 load ("helper/ClassLoader.js");
 load ("midi/ClassLoader.js");
 load ("daw/ClassLoader.js");
