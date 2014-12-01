a7e1bf4dd1820b69bb722299937a967aa494286a
Jürgen Moßgraber <juergen.mossgraber@online.de>
2014-12-01 13:58:21 +0100

    Added new Arranger functions and observers.

--- daw/ArrangerProxy.js
+++ daw/ArrangerProxy.js
@@ -10,10 +10,18 @@ function ArrangerProxy ()
     this.cueMarkersVisible = false;
     this.followsPlayback = false;
     this.largeTrackHeight = false;
+    this.clipLauncherVisible = false;
+    this.timelineVisible = false;
+    this.ioSectionVisible = false;
+    this.effectTracksVisible = false;
 
     this.arranger.areCueMarkersVisible ().addValueObserver (doObject (this, ArrangerProxy.prototype.handleCueMarkerVisibility));
     this.arranger.isPlaybackFollowEnabled ().addValueObserver (doObject (this, ArrangerProxy.prototype.handlePlaybackFollow));
     this.arranger.hasDoubleRowTrackHeight ().addValueObserver (doObject (this, ArrangerProxy.prototype.handleTrackRowHeight));
+    this.arranger.isClipLauncherVisible ().addValueObserver (doObject (this, ArrangerProxy.prototype.handleClipLauncherVisible));
+    this.arranger.isTimelineVisible ().addValueObserver (doObject (this, ArrangerProxy.prototype.handleTimelineVisible));
+    this.arranger.isIoSectionVisible ().addValueObserver (doObject (this, ArrangerProxy.prototype.handleIoSectionVisible));
+    this.arranger.areEffectTracksVisible ().addValueObserver (doObject (this, ArrangerProxy.prototype.handleEffectTracksVisible));
 }
 
 /**
@@ -67,6 +75,46 @@ ArrangerProxy.prototype.toggleTrackRowHeight = function ()
     this.arranger.hasDoubleRowTrackHeight ().toggle ();
 };
 
+ArrangerProxy.prototype.isClipLauncherVisible = function ()
+{
+    return this.clipLauncherVisible;
+};
+
+ArrangerProxy.prototype.isTimelineVisible = function ()
+{
+    return this.timelineVisible;
+};
+
+ArrangerProxy.prototype.isIoSectionVisible = function ()
+{
+    return this.ioSectionVisible;
+};
+
+ArrangerProxy.prototype.areEffectTracksVisible = function ()
+{
+    return this.effectTracksVisible;
+};
+
+ArrangerProxy.prototype.toggleClipLauncher = function ()
+{
+    this.arranger.isClipLauncherVisible ().toggle ();
+};
+
+ArrangerProxy.prototype.toggleTimeLine = function ()
+{
+    this.arranger.isTimelineVisible ().toggle ();
+};
+
+ArrangerProxy.prototype.toggleIoSection = function ()
+{
+    this.arranger.isIoSectionVisible ().toggle ();
+};
+
+ArrangerProxy.prototype.toggleEffectTracks = function ()
+{
+    this.arranger.areEffectTracksVisible ().toggle ();
+};
+
 //--------------------------------------
 // Callback Handlers
 //--------------------------------------
@@ -84,4 +132,24 @@ ArrangerProxy.prototype.handlePlaybackFollow = function (on)
 ArrangerProxy.prototype.handleTrackRowHeight = function (on)
 {
     this.largeTrackHeight = on;
+};
+
+ArrangerProxy.prototype.handleClipLauncherVisible = function (on)
+{
+    this.clipLauncherVisible = on;
+};
+    
+ArrangerProxy.prototype.handleTimelineVisible = function (on)
+{
+    this.timelineVisible = on;
+};
+    
+ArrangerProxy.prototype.handleIoSectionVisible = function (on)
+{
+    this.ioSectionVisible = on;
+};
+    
+ArrangerProxy.prototype.handleEffectTracksVisible = function (on)
+{
+    this.effectTracksVisible = on;
 };
